d79957467138b3290b9b14ef2cee58f4d525e04e
Mohamed Mahmoud <106276397+Mohammed-Mahmoud-Elsayed-Ahmed-MMES@users.noreply.github.com>
2026-04-08 08:21:58 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -12,7 +12,7 @@ const SUPABASE_URL     = 'https://loxcubuubveqgtsglllq.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxveGN1YnV1YnZlcWd0c2dsbGxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzMjMxMTIsImV4cCI6MjA4Nzg5OTExMn0.oFf2oHcY1kroPl91Zew2guaXZAcSpGeHA8HleJz_zf0';
 
 const GEMINI_API_KEY = 'AIzaSyBYAtJLkgbtlpmtsvn3eLankfdJxcsWprg';
-const GROQ_API_KEY   = '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';
+const GROQ_API_KEY   = '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';
 
 const { createClient } = supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
